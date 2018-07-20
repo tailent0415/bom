d3828de29e527dba317898f9930c1d61e7f5e74c
tailent0415 <40162330+tailent0415@users.noreply.github.com>
2018-07-20 10:26:22 +0800

    update
    Record function test conv

---
+++ scripts/record.js
@@ -0,0 +1,26 @@
+/* record table control */
+function delete_record(){
+	var sel_target = new Array();
+	var del_index = new Array();
+	sel_target = product_table.bootstrapTable('getSelections');
+	for( var i=0; i<sel_target.length; i++ ){
+		del_index[i] = sel_target[i].index
+	}
+	var json_array = convert_json_array( del_index );
+	var attr = {
+		"state": "rem_record_data",
+		"del_index": json_array
+	};
+	send_to_db( attr );
+}
+
+
+
+
+
+function receive_record(){
+	var attr = {
+		"state": "get_record_data",
+	};
+	receive_to_db( attr );
+}
