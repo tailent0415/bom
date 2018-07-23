7adce86a9d97c5060029fa017482ed13ae6a7d1f
tailent0415 <40162330+tailent0415@users.noreply.github.com>
2018-07-23 08:20:09 +0800

    update
    update google login sub function

--- scripts/google_login.js
+++ scripts/google_login.js
@@ -1,4 +1,18 @@
-/* google login event */
+// google login render icon
+function renderButton(){
+	gapi.signin2.render('my-signin2', {
+		'scope': 'profile email',
+		'width': 180,
+		'height': 32,
+		'longtitle': true,
+		'theme': 'dark',
+		'onsuccess': onSuccess,
+		'onfailure': onFailure
+	});
+};
+
+
+// google login event
 function onSuccess(googleUser){
 	user_email = googleUser.getBasicProfile().getEmail();
 	user_id = user_email.substring( 0, user_email.indexOf("@") );
@@ -9,7 +23,9 @@ function onSuccess(googleUser){
 };
 
 
-/* google login failure report */
+// google login failure report
 function onFailure(error){
 	alert(error);
 };
+
+
